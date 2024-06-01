6f47a01f8f33c7e7c7dfd159f4d6fdd15323106f
abhishek92110 <at604281@gmail.com>
2024-06-01 11:49:23 +0530

    initial commit

---
+++ models/Counselor.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const CounselorSchema = new mongoose.Schema({
+
+
+    Name: {
+        type: String,
+    },
+
+    lastName: {
+        type: String,
+
+    },
+    Number: {
+        type: String,
+
+    },
+    Email: {
+        type: String,
+
+
+    },
+    password: {
+        type: String,
+
+
+    },
+    Address: {
+        type: String,
+
+    },
+
+    file: {
+        type: String,
+
+    },
+    counselorNo: {
+        type: String,
+
+    },
+    counselorNumber: {
+        type: String,
+
+    },
+    
+    counselorReference: {
+        type: String,
+
+    },
+    url: {
+        type: String,
+
+    }
+
+});
+
+const counselors = new mongoose.model("counselors", CounselorSchema);
+
+
+module.exports = counselors;
